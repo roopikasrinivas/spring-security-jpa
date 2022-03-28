9c8cd6c5f72d562ba3efcd65b5dc5bcde9bee1af
Roopika Srinivas <roopika.srinivas@hpe.com>
2022-03-28 18:08:59 +0530

    JPA update

--- src/main/java/com/example/springsecurityjpa/MyUserDetailsService.java
+++ src/main/java/com/example/springsecurityjpa/MyUserDetailsService.java
@@ -1,14 +1,26 @@
 package com.example.springsecurityjpa;
 
+import com.example.springsecurityjpa.models.MyUserDetails;
+import com.example.springsecurityjpa.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return new MyUserDetails(username);
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        //return new MyUserDetails(username);
+        Optional<User> user = userRepository.findByUserName(userName);
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found: "+userName));
+        return user.map(MyUserDetails::new).get();
     }
 }
--- src/main/java/com/example/springsecurityjpa/SecurityConfiguration.java
+++ src/main/java/com/example/springsecurityjpa/SecurityConfiguration.java
@@ -25,7 +25,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/admin").hasRole("ADMIN")
-                .antMatchers("/user").hasAnyRole("USER","ADMIN")
+                .antMatchers("/user").hasAnyRole("ADMIN","USER")
                 .antMatchers("/").permitAll()
                 .and().formLogin();
     }
